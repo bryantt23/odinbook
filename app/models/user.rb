aa6538213806c3598bf1c4bae7e96363ebf56ace
bryant tunbutr <bryantt23@users.noreply.github.com>
2026-05-21 09:50:04 -0700

    Add post likes model and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :likes
+  has_many :post_likes, through: :likes, source: :post
 end
