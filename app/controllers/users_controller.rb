97337150c1a015685605a5f33ab830cfc3864d5d
bryant tunbutr <bryantt23@users.noreply.github.com>
2026-06-15 16:31:43 +0800

    show following & followed by

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user=User.find(params[:id])
+    @inverse_friendships=@user.inverse_friendships.includes(:user)
+    @friendships=@user.friendships.includes(:user)
   end
 end
