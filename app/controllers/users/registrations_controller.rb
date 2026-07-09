aa6d1a28f352766f8396175fa741e8ccf8a3929d
bryant tunbutr <bryantt23@users.noreply.github.com>
2026-07-09 23:30:26 +0800

    remove unneeded code

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # POST /users
   def create
     super do |user|
-      UserMailer.welcome_email(user).deliver_now
+      UserMailer.welcome_email(user).deliver_now if user.persisted?
     end
   end
 end
