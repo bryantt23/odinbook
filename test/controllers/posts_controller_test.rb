618df69f0a54b46ebc4b85d797cb1b80e5f6aea5
bryant tunbutr <bryantt23@users.noreply.github.com>
2026-07-26 08:55:32 +0800

    add show post redirect test

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -2,22 +2,23 @@ require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
-    get posts_show_url
-    assert_response :success
+    post=posts(:one)
+    get post_path(post)
+    assert_redirected_to new_user_session_path
   end
 
-  test "should get index" do
-    get posts_index_url
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get posts_index_url
+  #   assert_response :success
+  # end
 
-  test "should get edit" do
-    get posts_edit_url
-    assert_response :success
-  end
+  # test "should get edit" do
+  #   get posts_edit_url
+  #   assert_response :success
+  # end
 
-  test "should get new" do
-    get posts_new_url
-    assert_response :success
-  end
+  # test "should get new" do
+  #   get posts_new_url
+  #   assert_response :success
+  # end
 end
