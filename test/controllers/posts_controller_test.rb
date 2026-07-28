e03300c6aece528ec644554dbbd0c438b4a15bc2
bryant tunbutr <bryantt23@users.noreply.github.com>
2026-07-28 11:40:07 +0800

    add logged in user test

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,10 +1,22 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   test "should get show" do
     post=posts(:one)
     get post_path(post)
     assert_redirected_to new_user_session_path
+  end
+
+  test "GET show" do
+    # Use the sign_in helper to sign in a fixture `User` record.
+    sign_in users(:one)
+
+    post=posts(:one)
+    get post_path(post)
+
+    # assert something
+    assert_response :success
   end
 
   # test "should get index" do
