2b8a406f7b4b711f7afdaa7470aaf7f6353db080
bryantt23 <bryantt23@gmail.com>
2026-08-03 03:40:32 +0800

    skip welcome email in production

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # POST /users
   def create
     super do |user|
-      UserMailer.welcome_email(user).deliver_now if user.persisted?
+      UserMailer.welcome_email(user).deliver_now if Rails.env.development?
     end
   end
 end
