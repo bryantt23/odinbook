4be74f54022bdd62d5b81a9a67f82d245fb85b69
bryantt23 <bryantt23@gmail.com>
2026-08-05 14:14:29 +0800

    fix lint issues

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many :likes 
+  has_many :likes
   has_many :users_who_liked, through: :likes, source: :user
 end
